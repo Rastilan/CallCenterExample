8257ffce69ef4b8fef70603fa56a1846ca573838
Rastilan <christoperbhall@gmail.com>
2022-05-03 22:51:53 -0400

    updated select script

--- main.js
+++ main.js
@@ -1,55 +1,266 @@
-currentQuestion = "";
-currentA = "Yes";
-currentB = "No";
+let currentQuestion = 2;
+let questionText = "";
+let currentAnswer = "";
+let currentA = "";
+let currentB = "";
+let endScript = "";
 
-questionSet = "";
-
-function NextStep(currentQuestion, currentA, currentB) {
+// This function prints the new DOM elements (The new question and its answers)
+function NextStep() {
     const currentDiv = document.getElementById("content");
     let nextQuestion = document.createElement("div");
     let nextAnswerContainer = document.createElement("div");
     let answerOne = document.createElement("button");
     let answerTwo = document.createElement("button");
     nextAnswerContainer.setAttribute("class", "answer-container");
-    nextQuestion.textContent = currentQuestion;
+    nextQuestion.textContent = questionText;
     answerOne.textContent = currentA;
     answerTwo.textContent = currentB;
     answerOne.setAttribute("onclick", "Answer('A')");
     answerTwo.setAttribute("onclick", "Answer('B')");
     currentDiv.insertAdjacentElement('beforeend', nextQuestion);
-    currentDiv.insertAdjacentElement('beforeend', nextAnswerContainer)
-    nextAnswerContainer.insertAdjacentElement('beforeend', answerOne);
-    nextAnswerContainer.insertAdjacentElement('beforeend', answerTwo);
+    if(currentA !== "" || currentB !== ""){
+        currentDiv.insertAdjacentElement('beforeend', nextAnswerContainer);
+        nextAnswerContainer.insertAdjacentElement('beforeend', answerOne);
+        nextAnswerContainer.insertAdjacentElement('beforeend', answerTwo);
+    }
+    if(endScript !== ""){
+
+    }
+    
 }
-currentQuestionSelector(currentQuestion);
-NextStep(currentQuestion, currentA, currentB);
+// Sets our starting question and prints it to the page
+SetQuestionText(currentQuestion);
+NextStep();
 
 function Answer(val) {
     if(val == "A"){
-        currentQuestion = currentQuestionSelector(currentQuestion, 'A');
-        currentA = "A YES";
-        currentB = "A NO";
-        NextStep(currentQuestion, currentA, currentB);
+        CurrentQuestionSelector(currentQuestion, 'A');
+        NextStep();
     }
     else if(val == "B"){
-        currentQuestion = currentQuestionSelector(currentQuestion, 'B');
-        currentA = "B YES";
-        currentB = "B NO";
-        NextStep(currentQuestion, currentA, currentB);
+        CurrentQuestionSelector(currentQuestion, 'B');
+        NextStep();
     }
     else {
-
+        console.log(val + " this incorrect value loaded into the Answer function");
     }
 }
-function currentQuestionSelector(question, answer) {
+function CurrentQuestionSelector(question, answer) {
     switch (question) {
-        case "":
-            currentQuestion = "We see that you recently showed interest in getting out of debt but may not have qualified. Is that something you’re still interested in?"
+        case 1:
+            switch (answer) {
+                case "A":
+                    currentQuestion = 2;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion;
+
+                case "B":
+                    currentQuestion = 3;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+            }
+        case 2:
+            switch (answer) {
+                case "A":
+                    currentQuestion = 6;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion;
+
+                case "B":
+                    currentQuestion = 11;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+            }
+        case 3:
+            switch(answer){
+                case "A":
+                    currentQuestion = 4;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+                case "B":
+                    currentQuestion = 5;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+            }
+        case 4:
+            switch(answer){
+                case "A":
+                    currentQuestion = 7;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+                case "B":
+                    currentQuestion = 5;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+                    
+            }
+        case 5:
+            switch(answer){
+                case "A":
+                    currentQuestion = 8;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+                case "B":
+                    currentQuestion = 11;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;  
+            }
+        case 6:
+            switch(answer){
+                case "A":
+                    currentQuestion = 5;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, SetQuestionText(currentQuestion);
+                case "B":
+                    currentQuestion = 8;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+            }
+        case 7:
             break;
-        case "We see that you recently showed interest in getting out of debt but may not have qualified. Is that something you’re still interested in?":
-            return currentQuestion = "Do you have at least $3,000 of debt or more?";
-            break;
+        case 8:
+        switch(answer){
+            case "A":
+                currentQuestion = 9;
+                SetQuestionText(currentQuestion);
+                return currentQuestion, questionText;
+            case "B":
+                currentQuestion = 10;
+                SetQuestionText(currentQuestion);
+                return currentQuestion, questionText;     
+        }
+        case 9:
+        switch(answer){
+            case "A":
+
+            case "B":
+                
+        }
+        case 10:
+        switch(answer){
+            case "A":
+
+            case "B":
+                
+        }
+        case 11:
+            switch(answer){
+                case "A":
+                    currentQuestion = 12;
+                    questionText = SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+                case "B":
+                    currentQuestion = 13;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+            }
+        case 12:
+            switch(answer){
+                case "A":
+                    currentQuestion = 14;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+                case "B":
+                    currentQuestion = 13;
+                    SetQuestionText(currentQuestion);
+                    return currentQuestion, questionText;
+            }
+        case 13:
+            switch(answer){
+                case "A":
+    
+                case "B":
+                    
+            }
+        case 14:
+            switch(answer){
+                case "A":
+    
+                case "B":
+                    
+            }
+        case 10:
+            switch(answer){
+                case "A":
+    
+                case "B":
+                    
+            }
+                    
         default: ""
 
     }
+}
+
+function SetQuestionText(currentQuestionVal) {
+    switch (currentQuestionVal) {
+        case 0:
+            return questionText = "Case 0";
+        case 1:
+            currentA = "DQ";
+            currentB = "<10k";
+            return questionText = "Determine Lead Type (DQ / <10K)";
+        case 2:
+            currentA = "Yes";
+            currentB = "No";
+            questionText = "We see that you recently showed interest in getting out of debt but may not have qualified. Is that something you’re still interested in?";
+            break;
+        case 3: 
+            currentA = "More than 10k";
+            currentB = "Less than 10k";
+            return questionText = "Do you know approximately how much debt you have?";
+        case 4: 
+            currentA = "Yes";
+            currentB = "No";
+            questionText = "Are you currently paying your bills on-time right now?";
+            break;
+        case 5: 
+            currentA = "Yes";
+            currentB = "No";
+            return questionText = "Are you looking to potentially lower your monthly payments or need some help in paying the debt off?";   
+        case 6: 
+            currentA = "Yes";
+            currentB = "No";
+            return questionText = "Do you have at least $3,000 of debt or more?";
+        case 7: 
+            currentA = "";
+            currentB = "";
+            return questionText = "Good news! It looks like we may be able to help you. Bare with me one moment while I get you transferred over to a specialist that can assist ";
+        case 8: 
+            currentA = "Yes";
+            currentB = "No";
+            return questionText = "Is this credit card or medical debt?";
+        case 9: 
+            currentA = "";
+            currentB = "";
+            return questionText = "";
+        case 10: 
+            currentA = "";
+            currentB = "";
+            return questionText = "End Call [NQ Wrong Debt] – Sorry we couldn’t help you out today. I do appreciate you taking the time with me today! Have a great day!";
+        case 11: 
+            currentA = "Yes";
+            currentB = "No";
+            return questionText = "OK, do you know if there is any damage in your credit history that you disagree with or feel is inaccurate?";
+        case 12: 
+            currentA = "Yes";
+            currentB = "No";
+            return questionText = "Are you currently employed?";
+        case 13: 
+            currentA = "";
+            currentB = "";
+            return questionText = "End Call [NQ No Damage Credit History]– Sorry we couldn’t help you out today. I do appreciate you taking the time with me today! Have a great day!";
+        case 14: 
+            currentA = "";
+            currentB = "";
+            return questionText = " Since your credit is holding you back from achieving your financial goals, I think a credit restoration company might be able to help.Give me a second while I get them on the line for you.";
+
+
+
+
+
+
+
+        }
 }
