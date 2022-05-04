ca8a42811b00f6fcb896fbb6f7104b5b9c0fda40
Rastilan <christoperbhall@gmail.com>
2022-05-04 00:59:42 -0400

    removed endingScripts variable, removed bug that required it to handle multiple lines

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let currentQuestion = 2;
-let questionText = "";
+let questionText = [];
 let currentAnswer = "";
 let currentA = "";
 let currentB = "";
-let endingScripts = [];
+
 
 // This function prints the new DOM elements (The new question and its answers)
 function NextStep() {
@@ -24,7 +24,7 @@ function NextStep() {
     }
 
     
-    let nextQuestion = document.createElement("div");
+    
     let nextAnswerContainer = document.createElement("div");
     let answerOne = document.createElement("button");
     let answerTwo = document.createElement("button");
@@ -38,21 +38,20 @@ function NextStep() {
     answerTwo.setAttribute("id", "answerB");
  
     // Next Question text creation and placement
-    nextQuestion.textContent = questionText;
-    nextQuestion.setAttribute("class", "question");
-    currentDiv.insertAdjacentElement('beforeend', nextQuestion);
+    if(questionText.length > 0){
+        for(let i = 0; i < questionText.length; i++) {
+            let nextQuestion = document.createElement("div");
+            nextQuestion.textContent = questionText[i];
+            nextQuestion.setAttribute("class", "question");
+            currentDiv.insertAdjacentElement('beforeend', nextQuestion);
+        }
+    }
+    
     
     if(currentA !== "" || currentB !== ""){
         currentDiv.insertAdjacentElement('beforeend', nextAnswerContainer);
         nextAnswerContainer.insertAdjacentElement('beforeend', answerOne);
         nextAnswerContainer.insertAdjacentElement('beforeend', answerTwo);
-    }
-    if(endingScripts.length > 0){
-        for(let i = 0; i <= endingScripts.length; i++) {
-            let nextEndingScript = document.createElement("div");
-            nextEndingScript.textContent = endingScripts[i];
-            currentDiv.insertAdjacentElement('beforeend', nextEndingScript);
-        }
     }
 
     
@@ -202,72 +201,72 @@ function CurrentQuestionSelector(question, answer) {
 function SetQuestionText(currentQuestionVal) {
     switch (currentQuestionVal) {
         case 0:
-            return questionText = "Case 0";
+            return questionText[0] ="Case 0";
         case 1:
             currentA = "DQ";
             currentB = "<10k";
-            return questionText = "Determine Lead Type (DQ / <10K)";
+            return questionText[0] ="Determine Lead Type (DQ / <10K)";
         case 2:
             currentA = "Yes";
             currentB = "No";
-            questionText = "We see that you recently showed interest in getting out of debt but may not have qualified. Is that something you’re still interested in?";
+            questionText[0] ="We see that you recently showed interest in getting out of debt but may not have qualified. Is that something you’re still interested in?";
             break;
         case 3: 
             currentA = "More than 10k";
             currentB = "Less than 10k";
-            return questionText = "Do you know approximately how much debt you have?";
+            return questionText[0] ="Do you know approximately how much debt you have?";
         case 4: 
             currentA = "Yes";
             currentB = "No";
-            questionText = "Are you currently paying your bills on-time right now?";
+            questionText[0] ="Are you currently paying your bills on-time right now?";
             break;
         case 5: 
             currentA = "Yes";
             currentB = "No";
-            return questionText = "Are you looking to potentially lower your monthly payments or need some help in paying the debt off?";   
+            return questionText[0] ="Are you looking to potentially lower your monthly payments or need some help in paying the debt off?";   
         case 6: 
             currentA = "Yes";
             currentB = "No";
-            return questionText = "Do you have at least $3,000 of debt or more?";
+            return questionText[0] ="Do you have at least $3,000 of debt or more?";
         case 7: 
             currentA = "";
             currentB = "";
-            return questionText = "Good news! It looks like we may be able to help you. Bare with me one moment while I get you transferred over to a specialist that can assist ";
+            return questionText[0] ="Good news! It looks like we may be able to help you. Bare with me one moment while I get you transferred over to a specialist that can assist ";
         case 8: 
             currentA = "Yes";
             currentB = "No";
-            return questionText = "Is this credit card or medical debt?";
+            return questionText[0] ="Is this credit card or medical debt?";
         case 9: 
             currentA = "";
             currentB = "";
-            questionText = "Great, it sounds like you could be an ideal candidate for our non-profit Debt Management Program.";
-            endingScripts[0] = "Now, this option would combine your credit cards and other unsecured debt into one low affordable monthly payment at a much lower interest rate you have now.";
-            endingScripts[1] = "On this program you may have to close out some of your cards but most of our clients don't have a problem with that since all they want to do is pay them off anyways.";
-            endingScripts[2] = "What I am going to do is get you connected with a partner who specializes in this so they can give you a payment and quote, okay?";
-            endingScripts[3] = "Just one sec while I get my associate on the line with us.Transfer Number = TBD";
+            questionText[0] ="Great, it sounds like you could be an ideal candidate for our non-profit Debt Management Program.";
+            questionText[1] = "Now, this option would combine your credit cards and other unsecured debt into one low affordable monthly payment at a much lower interest rate you have now.";
+            questionText[2] = "On this program you may have to close out some of your cards but most of our clients don't have a problem with that since all they want to do is pay them off anyways.";
+            questionText[3] = "What I am going to do is get you connected with a partner who specializes in this so they can give you a payment and quote, okay?";
+            questionText[4] = "Just one sec while I get my associate on the line with us.Transfer Number = TBD";
             break;
         case 10: 
             currentA = "";
             currentB = "";
-            return questionText = "End Call [NQ Wrong Debt] – Sorry we couldn’t help you out today. I do appreciate you taking the time with me today! Have a great day!";
+            return questionText[0] ="End Call [NQ Wrong Debt] – Sorry we couldn’t help you out today. I do appreciate you taking the time with me today! Have a great day!";
         case 11: 
             currentA = "Yes";
             currentB = "No";
-            return questionText = "OK, do you know if there is any damage in your credit history that you disagree with or feel is inaccurate?";
+            return questionText[0] ="OK, do you know if there is any damage in your credit history that you disagree with or feel is inaccurate?";
         case 12: 
             currentA = "Yes";
             currentB = "No";
-            return questionText = "Are you currently employed?";
+            return questionText[0] ="Are you currently employed?";
         case 13: 
             currentA = "";
             currentB = "";
-            return questionText = "End Call [NQ No Damage Credit History]– Sorry we couldn’t help you out today. I do appreciate you taking the time with me today! Have a great day!";
+            return questionText[0] ="End Call [NQ No Damage Credit History]– Sorry we couldn’t help you out today. I do appreciate you taking the time with me today! Have a great day!";
         case 14: 
             currentA = "";
             currentB = "";
-            questionText = " Since your credit is holding you back from achieving your financial goals, I think a credit restoration company might be able to help. Give me a second while I get them on the line for you.";
-            endingScripts[0] = "Hi, I have a customer on the line who’s interested in credit repair and pre-qualified for your program.";
-            endingScripts[1] = "I’ll let you take over the call from here and they will take excellent care of you. Thanks";
+            questionText[0] =" Since your credit is holding you back from achieving your financial goals, I think a credit restoration company might be able to help. Give me a second while I get them on the line for you.";
+            questionText[1] = "Hi, I have a customer on the line who’s interested in credit repair and pre-qualified for your program.";
+            questionText[2] = "I’ll let you take over the call from here and they will take excellent care of you. Thanks";
 
 
 
