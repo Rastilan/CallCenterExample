f95c4d94991c2d75f998d05793ac8416d1ec6fb3
Rastilan <christoperbhall@gmail.com>
2022-05-03 20:41:17 -0400

    style updated

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-currentQuestion = "We see that you recently showed interest in getting out of debt but may not have qualified. Is that something you’re still interested in?";
+currentQuestion = "";
 currentA = "Yes";
 currentB = "No";
+
+questionSet = "";
 
 function NextStep(currentQuestion, currentA, currentB) {
     const currentDiv = document.getElementById("content");
     let nextQuestion = document.createElement("div");
+    let nextAnswerContainer = document.createElement("div");
     let answerOne = document.createElement("button");
     let answerTwo = document.createElement("button");
+    nextAnswerContainer.setAttribute("class", "answer-container");
     nextQuestion.textContent = currentQuestion;
     answerOne.textContent = currentA;
     answerTwo.textContent = currentB;
     answerOne.setAttribute("onclick", "Answer('A')");
     answerTwo.setAttribute("onclick", "Answer('B')");
     currentDiv.insertAdjacentElement('beforeend', nextQuestion);
-    currentDiv.insertAdjacentElement('beforeend', answerOne);
-    currentDiv.insertAdjacentElement('beforeend', answerTwo);
+    currentDiv.insertAdjacentElement('beforeend', nextAnswerContainer)
+    nextAnswerContainer.insertAdjacentElement('beforeend', answerOne);
+    nextAnswerContainer.insertAdjacentElement('beforeend', answerTwo);
 }
+currentQuestionSelector(currentQuestion);
 NextStep(currentQuestion, currentA, currentB);
 
 function Answer(val) {
     if(val == "A"){
-        currentQuestion = "Do you have at least $3,000 of debt or more?";
+        currentQuestion = currentQuestionSelector(currentQuestion, 'A');
         currentA = "A YES";
         currentB = "A NO";
         NextStep(currentQuestion, currentA, currentB);
     }
     else if(val == "B"){
-        currentQuestion = "OK, do you know if there is any damage in your credit history that you disagree with or feel is inaccurate?";
+        currentQuestion = currentQuestionSelector(currentQuestion, 'B');
         currentA = "B YES";
         currentB = "B NO";
         NextStep(currentQuestion, currentA, currentB);
@@ -35,4 +41,15 @@ function Answer(val) {
 
     }
 }
+function currentQuestionSelector(question, answer) {
+    switch (question) {
+        case "":
+            currentQuestion = "We see that you recently showed interest in getting out of debt but may not have qualified. Is that something you’re still interested in?"
+            break;
+        case "We see that you recently showed interest in getting out of debt but may not have qualified. Is that something you’re still interested in?":
+            return currentQuestion = "Do you have at least $3,000 of debt or more?";
+            break;
+        default: ""
 
+    }
+}
